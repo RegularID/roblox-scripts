ce656f2c964dd5dc825f97eaac2943042e71d6f3
Elden <69537751+RegularID@users.noreply.github.com>
2021-12-13 23:30:34 +0200

    Create Teleport Bypass.lua
    Leaking this due to an update that made this useless also I have a new method, yes this replicates (obviously).

---
+++ The Streets/Teleport Bypass.lua
@@ -0,0 +1,26 @@
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+
+
+local Player = Players.LocalPlayer
+local Root = Player.Character.Humanoid.RootPart
+local Torso = Player.Character.Torso
+
+
+local Seat
+for _, v in ipairs(workspace:GetDescendants()) do
+    if v:IsA("Seat") and not v:FindFirstAncestor("Jeep", true) and not v.Occupant then
+        Seat = v
+        break
+    end
+end
+
+Root.RootJoint:Destroy()
+Root.Parent = workspace
+
+RunService.Stepped:Connect(function()
+    if Root then
+        firetouchinterest(Torso, Seat, 0)
+        firetouchinterest(Torso, Seat, 1)
+    end
+end)
