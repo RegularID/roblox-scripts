e190f1c32cfddb2aa6eb7b4bb6811802015d2a13
Elden <69537751+RegularID@users.noreply.github.com>
2021-12-29 18:29:45 +0200

    Small changes

--- The Streets/Teleport Bypass.lua
+++ The Streets/Teleport Bypass.lua
@@ -3,24 +3,53 @@ local RunService = game:GetService("RunService")
 
 
 local Player = Players.LocalPlayer
-local Root = Player.Character.Humanoid.RootPart
-local Torso = Player.Character.Torso
-
-
+local Character = Player.Character
+local Humanoid = Character.Humanoid
+local Root = Humanoid.RootPart
+local Torso = Character.Torso
+local Seats = {}
 local Seat
+local Loop
+
+
 for _, v in ipairs(workspace:GetDescendants()) do
-    if v:IsA("Seat") and not v:FindFirstAncestor("Jeep", true) and not v.Occupant then
+    if v:IsA("Seat") then
+        if v:FindFirstAncestor("Jeep", true) then continue end
+        table.insert(Seats, v)
+    end
+end
+if #Seats == 0 then return end
+
+
+function GetSeat()
+    for _, v in ipairs(Seats) do
+        if v.Occupant then continue end
         Seat = v
-        break
+        return v
     end
 end
 
+
+local SitDebounce = false
+function SitOnSeat()
+    if Character and Character.Parent == nil then return Loop:Disconnect() end
+    if not Torso or not Root or not Humanoid or not Character then return Loop:Disconnect() end
+    if Seat and not Seat.Occupant then
+        if SitDebounce then return end
+        SitDebounce = true
+        firetouchinterest(Torso, Seat, 0)
+        firetouchinterest(Torso, Seat, 1)
+        delay(0.5, function()
+            SitDebounce = false
+        end)
+    else
+        GetSeat()
+    end
+end
+
+
+SitOnSeat()
 Root.RootJoint:Destroy()
 Root.Parent = workspace
 
-RunService.Stepped:Connect(function()
-    if Root and Torso and Seat and firetouchinterest then
-        firetouchinterest(Torso, Seat, 0)
-        firetouchinterest(Torso, Seat, 1)
-    end
-end)
+Loop = RunService.Stepped:Connect(SitOnSeat)
