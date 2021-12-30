7fa207d05e071cfb10757458e121804084f888bc
Elden <69537751+RegularID@users.noreply.github.com>
2021-12-30 04:54:05 +0200

    fixed "RootPart" variable

--- The Streets/Auto Farm.lua
+++ The Streets/Auto Farm.lua
@@ -2,16 +2,18 @@ local SERVER_HOP = true -- Might get you a cooldown better to do it in a private
 local SERVER_HOP_DELAY = 10
 
 
-if not game:IsLoaded() then game.Loaded:Wait() end
-math.randomseed(os.clock()) -- I got unlucky with serverhop
-
-
 local wait = task.wait
 local delay = task.delay
 local spawn = task.spawn
 
 
+if not game:IsLoaded() then game.Loaded:Wait() end
+local Original = 455366377 or false
+if not Original and game.PlaceId ~= 4669040 then return end
+
+
 local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
 local VirtualUser = game:GetService("VirtualUser")
 local HttpService = game:GetService("HttpService")
 local TeleportService = game:GetService("TeleportService")
@@ -21,20 +23,29 @@ local Player = Players.LocalPlayer
 local Character = Player.Character or Player.CharacterAdded:Wait()
 local Backpack = Player:WaitForChild("Backpack")
 local Humanoid = Character:WaitForChild("Humanoid")
-local RootPart = Humanoid.RootPart
+local Root = Humanoid.RootPart
+
+
+function FireTouch(Part, Part2)
+    firetouchinterest(Part, Part2, 0)
+    firetouchinterest(Part, Part2, 1)
+end
+
+
+function Loop()
+    if Humanoid.Sit then Humanoid.Sit = false end
+end
 
 
 function OnWorkspaceChildAdded(self)
     delay(0, function()
-        pcall(function()
-            if tostring(self) == "RandomSpawner" then
-                while self and self.Parent == workspace do
-                    if Humanoid.Sit then Humanoid.Sit = false end
-                    RootPart.CFrame = self.CFrame
-                    wait()
-                end
+        if tostring(self) == "RandomSpawner" then
+            while self and self.Parent == workspace do
+                Root.CFrame = self.CFrame
+                FireTouch(Root, self)
+                wait()
             end
-        end)
+        end
     end)
 end
 
@@ -43,12 +54,13 @@ function OnBackpackChildAdded(self)
     if self:IsA("Tool") then
         if string.find(tostring(self), "Cash") then
             delay(0, function()
-                self.Parent = Character
+                Humanoid:EquipTool(self)
                 self:Activate()
             end)
         end
     end
 end
+
 
 function OnCharacterAdded(_Character)
     Character = _Character
@@ -68,26 +80,28 @@ end
 function ServerHop()
     pcall(function()
         local Servers = {}
-        local Url = game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")
+        local Url = game:HttpGetAsync("https://games.roblox.com/v1/games/" .. 455366377 .. "/servers/Public?sortOrder=Asc&limit=100")
         for _, v in ipairs(HttpService:JSONDecode(Url).data) do
             if typeof(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
                 Servers[#Servers + 1] = v.id
             end
         end
         if #Servers > 0 then
-            TeleportService:TeleportToPlaceInstance(game.PlaceId, Servers[math.random(1, #Servers)])
+            TeleportService:TeleportToPlaceInstance(455366377, Servers[math.random(1, #Servers)])
         end
     end)
     delay(5, ServerHop)
 end
 
 
+if not Original then ServerHop() end
 if SERVER_HOP then delay(SERVER_HOP_DELAY, ServerHop) end
 for _, v in ipairs(workspace:GetChildren()) do
     OnWorkspaceChildAdded(v)
 end
 
 
+RunService.Stepped:Connect(Loop)
 workspace.ChildAdded:Connect(OnWorkspaceChildAdded)
 Backpack.ChildAdded:Connect(OnBackpackChildAdded)
 Player.CharacterAdded:Connect(OnCharacterAdded)
